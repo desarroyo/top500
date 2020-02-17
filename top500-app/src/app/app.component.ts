7c0958ba5d2a83e5274a320949ebcc37aa1ce136
David Arroyo <chevy.arroyo@gmail.com>
2020-02-17 00:13:34 -0600

    avances front-end maquetado

---
+++ top500-app/src/app/app.component.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'top500-app';
+
+  ej_valor: number = 5;
+  title2 = "2000"
+
+  data2 = [
+    ['Firefox', 0, 7],
+    ['IE', 26.8, this.ej_valor],
+    ['Chrome', -3, 7],
+    ['Safari', 8.5, -5],
+    ['Opera', 6.2, 7],
+    ['Others', 0.7, -3]
+  ];
+
+  chart = {
+    title: "Top500 2020",
+    type: "PieChart",
+    data: [
+      ['Firefox', 45.0],
+      ['IE', 26.8],
+      ['Chrome', 12.8],
+      ['Safari', 8.5],
+      ['Opera', 6.2],
+      ['Others', 0.7]
+    ],
+    columnNames: ['Browser', 'Percentage'],
+    options: {
+      is3D: true,
+    }
+  }
+
+  lineChart = {
+    title: "Top500 2020",
+    type: "Line",
+    data: this.data2,
+    columnNames: ['Browser', 'Percentage', 'Expenses'],
+    options: {
+      chart: {
+        title: this.title2,
+        subtitle: 'in millions of dollars (USD)'
+      },
+      curveType: 'function',
+      legend: { position: 'bottom' },
+
+    }
+  }
+
+
+  clickEvent() {
+
+    (async () => {
+      // Do something before delay
+      console.log('before delay')
+
+
+
+      for (let i = 0; i < 3; i++) {
+        await this.delay(1000);
+        // Do something after
+        console.log('after delay')
+        this.ej_valor += 2;
+        console.log(this.ej_valor);
+        this.lineChart.options.chart.title = "Año: "+ (2000+ i);
+
+        this.data2 = [
+          ['Firefox', 0, 7],
+          ['IE', 26.8, this.ej_valor],
+          ['Chrome', -3, 7],
+          ['Safari', 8.5, -5],
+          ['Opera', 6.2, 7],
+          ['Others', 0.7, -3]
+        ];
+
+      }
+
+
+
+    })();
+
+
+  }
+
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+}
---
+++ top500-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GoogleChartsModule } from 'angular-google-charts';
+
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    GoogleChartsModule.forRoot(),
+
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
