bd05cead952941ddcfbbc405ef92d3af8409e0a7
David Arroyo <chevy.arroyo@gmail.com>
2020-03-21 22:41:17 -0600

    Mapas y más Gráficas

--- top500-api/dj_top500/urls.py
+++ top500-api/dj_top500/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import categoria_list,categoria_detalle, top500_totales_list, top500_crecimientos_list, top500_crecimiento
- 
+from .views import categoria_list,categoria_detalle, top500_totales_list, top500_crecimientos_list, top500_crecimiento, top500_continentes, top500_countries, top500_os, top500_os, top500_manufacturer, top500_architecture, top500_processor, top500_mhz, top500_rmax
+
 urlpatterns = [
     path('categorias/', categoria_list,name='categoria_list' ),
     path('categorias/<int:pk>', categoria_detalle,name='categoria_detalle' ),
     path('totales/', top500_totales_list,name='top500_totales_list' ),
     path('crecimientos/', top500_crecimientos_list,name='top500_crecimientos_list' ),
     path('crecimiento/<str:pk>', top500_crecimiento,name='top500_crecimiento' ),
+    path('continentes/', top500_continentes,name='top500_continentes' ),
+    path('countries/', top500_countries,name='top500_countries' ),
+    path('os/', top500_os,name='top500_os' ), 
+	path('manufacturer/', top500_manufacturer,name='top500_manufacturer' ), 
+	path('architecture/', top500_architecture,name='top500_architecture' ), 
+	path('processor/', top500_processor,name='top500_processor' ), 
+	path('mhz/', top500_mhz,name='top500_mhz' ), 
+	path('rmax/', top500_rmax,name='top500_rmax' ),
 ]
--- top500-api/dj_top500/views.py
+++ top500-api/dj_top500/views.py
@@ -31,6 +31,94 @@ FROM
  -- WHERE countries.country in ('Colombia')
 
 '''
+
+_QUERY_CONTINENT = '''
+select y.rank_year as year,c.code, c.name as continent, coalesce(total,0) as total
+from continents c 
+cross join ( select distinct rankings.rank_year from rankings ) y
+
+left outer join (
+  select rank_year,continent ,count(*) as total
+  from rankings
+  group by rank_year, continent
+)con on con.continent = c.name and y.rank_year = con.rank_year
+order by y.rank_year, c.name
+'''
+
+_QUERY_COUNTRY = '''
+select rank_year as year,country ,count(*) as total
+from rankings
+group by rank_year, country
+ORDER BY rank_year,country
+'''
+
+
+_QUERY_OS = '''
+select rank_year as year, os, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, os) num ,rank_year, os ,count(*) as total
+from rankings
+group by rank_year, os
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
+_QUERY_MANUFACTURER = '''
+select rank_year as year, manufacturer, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, manufacturer) num ,rank_year, manufacturer ,count(*) as total
+from rankings
+group by rank_year, manufacturer
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
+_QUERY_ARCHITECTURE = '''
+select rank_year as year, architecture, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, architecture) num ,rank_year, architecture ,count(*) as total
+from rankings
+group by rank_year, architecture
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
+_QUERY_PROCESSOR = '''
+select rank_year as year, processor, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, processor) num ,rank_year, processor ,count(*) as total
+from rankings
+group by rank_year, processor
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
+_QUERY_MHZ = '''
+select rank_year as year, mhz, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, mhz) num ,rank_year, mhz ,count(*) as total
+from rankings
+group by rank_year, mhz
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
+_QUERY_RMAX = '''
+select rank_year as year, rmax, total 
+from (
+select  row_number() over(partition by rank_year order by rank_year, rmax) num ,rank_year, rmax ,count(*) as total
+from rankings
+group by rank_year, rmax
+)con
+where num <= 15
+ORDER BY rank_year asc,total desc
+'''
+
 _QUERY_PART_3 = '''
 
    GROUP BY countries.country,
@@ -99,6 +187,274 @@ def top500_crecimiento(request,pk):
 
     j = json.loads(ds_cc.to_json(orient='split'))
     return JsonResponse(j) 
+
+def top500_continentes(request):
+    
+    df = pd.read_sql_query(_QUERY_CONTINENT, connection)
+
+    ds_cc = df
+    ds_cc = ds_cc.set_index('year')
+    # del ds_cc['crecimiento']
+
+    ds_cc = ds_cc.sort_index()
+    #ds_cc = ds_cc.pivot(columns='continent', values='total')
+
+    grouped = ds_cc.groupby(level=0).apply(lambda x: json.loads(x.to_json(orient='split'))) 
+    data = []
+
+    for el in grouped.values:
+        data.append({
+            'year' : el["index"][0],
+            'data' : el["data"]
+        })
+
+    j = {'data' : data}
+
+    #j = json.loads(grouped.to_json(orient='split'))
+    return JsonResponse(j)   
+
+
+def top500_countries(request):
+    
+    df = pd.read_sql_query(_QUERY_COUNTRY, connection)
+
+    ds_cc = df
+    ds_cc = ds_cc.set_index('year')
+
+    ds_cc = ds_cc.sort_index()
+
+    grouped = ds_cc.groupby(level=0).apply(lambda x: json.loads(x.to_json(orient='split'))) 
+    data = []
+
+    for el in grouped.values:
+        data.append({
+            'year' : el["index"][0],
+            'data' : el["data"]
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+
+
+def top500_os(request):
+    
+    df = pd.read_sql_query(_QUERY_OS, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='os', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='os', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)
+
+
+def top500_os(request):
+    
+    df = pd.read_sql_query(_QUERY_OS, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='os', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='os', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
+def top500_manufacturer(request):
+    
+    df = pd.read_sql_query(_QUERY_MANUFACTURER, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='manufacturer', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='manufacturer', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
+def top500_architecture(request):
+    
+    df = pd.read_sql_query(_QUERY_ARCHITECTURE, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='architecture', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='architecture', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
+def top500_processor(request):
+    
+    df = pd.read_sql_query(_QUERY_PROCESSOR, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='processor', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='processor', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
+def top500_mhz(request):
+    
+    df = pd.read_sql_query(_QUERY_MHZ, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='mhz', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='mhz', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
+def top500_rmax(request):
+    
+    df = pd.read_sql_query(_QUERY_RMAX, connection)
+
+    data = []
+
+    ds_cc = df
+    piv = ds_cc.set_index('year')
+    piv = piv.sort_index()
+    piv = piv.pivot(columns='rmax', values='total')
+
+    for i in range(0,len(piv.index)):
+        #print(piv.index[i])
+        p0 = ds_cc.loc[ds_cc['year'] == piv.index[i]]    
+        p0 = p0.set_index('year')
+        #p0 = p0.sort_values(by=['total'], ascending=False)
+        p0 = p0.pivot(columns='rmax', values='total')
+        
+        t = []
+        for e in range(0,len(p0.columns)):
+            t.append([p0.columns[e], p0.values[0][e].item()])
+        
+        data.append({
+            'year' : piv.index[i].item(),
+            'data' : t
+        })
+
+    j = {'data' : data}
+
+    return JsonResponse(j)  
+    
 
 def categoria_list(request):
     MAX_OBJECTS = 20
